202b9dc8cf9b19a32eb00a76bd057bd237760041
Steven Wu <stevenwu@mirle.com.tw>
2022-01-05 09:41:33 +0800

    Initial Project

---
+++ src/main/java/tw/com/pluto/trace/security/TraceSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package tw.com.pluto.trace.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraceSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/com/pluto/trace/security/TraceSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.pluto.trace.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
